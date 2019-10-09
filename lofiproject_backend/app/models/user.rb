65e76da25626e2b80436e82fa35cde37b2992889
Tommy Liang <tommyliangsays@gmail.com>
2019-10-09 14:39:12 -0400

    High Priority Complete

--- lofiproject_backend/app/models/user.rb
+++ lofiproject_backend/app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   has_and_belongs_to_many :songs
 
   validates :username, presence: true, uniqueness: true
-  validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }, on: :create
 end
