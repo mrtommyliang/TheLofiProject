8ff8d4c348a3576be1dcdd12a969189b28295e48
Tommy Liang <tommyliangsays@gmail.com>
2019-10-09 15:55:49 -0400

    Fixed login and register

--- lofiproject_backend/app/controllers/users_controller.rb
+++ lofiproject_backend/app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
   before_action :authorize_request, except: [:create,:update]
-  before_action :authenticate_user!, except: [:new, :update]
 
   # GET /users
   def index
