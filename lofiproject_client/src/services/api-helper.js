1a598c1398488f53cec38e69fb640c9d75e0d1b4
Tommy Liang <tommyliangsays@gmail.com>
2019-10-11 12:32:13 -0400

    updates

--- lofiproject_client/src/services/api-helper.js
+++ lofiproject_client/src/services/api-helper.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 const api = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: 'http://lofiproj.herokuapp.com'
 });
 
 export const loginUser = async (loginData) => {
